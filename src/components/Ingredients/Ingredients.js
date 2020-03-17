fd5f186e9fb717034d26377d9ef0f99321133bb0
hasheebkazi <hasheeb.k@gmail.com>
2020-03-17 12:58:55 -0400

    project setup + learn useState hook

---
+++ src/components/Ingredients/Ingredients.js
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+
+import IngredientForm from './IngredientForm';
+import IngredientList from './IngredientList';
+import Search from './Search';
+
+function Ingredients() {
+
+    const [userIngredients, updateIngredientsState] = useState([]);
+
+    const addIngredientHandler = ingredient => {
+        updateIngredientsState(prevIngredients => [...prevIngredients, { id: Math.random().toString(), ...ingredient }]);
+    };
+
+    return (
+        <div className="App">
+            <IngredientForm onAddIngredient={ addIngredientHandler } />
+
+            <section>
+                <Search />
+                <IngredientList ingredients={ userIngredients } onRemoveItem={() => {}} />
+            </section>
+        </div>
+    );
+}
+
+export default Ingredients;
